3fda54316eae5328e15189be3fbdd765e51a180d
tanyaner96 <tanyaner96@users.noreply.github.com>
2020-06-30 12:45:07 +0000

    automated commit by submit50

--- hello.c
+++ hello.c
@@ -3,6 +3,8 @@
 
 int main(void)
 {
+    //Asks user for their name
     string name = get_string("May I inquire as to how I should address you, brave adventurer?\n");
+    //Returns name of user
     printf("Well hello there, %s! Welcome to CS50!\n", name);
 }

a4385c252cde2ee9d108773cb859ecff86d6d94f
tanyaner96 <tanyaner96@users.noreply.github.com>
2021-01-16 15:04:17 +0000

    automated commit by check50 [check50=True]

--- runoff.c
+++ runoff.c
@@ -192,13 +192,13 @@ int find_min(void)
 {
     // TODO
     // return the minimum vote total for any candidate who is still in the election
-    int min;
+    int min = voter_count + 1;
     
     for (int i = 0; i < candidate_count; i++)
     {
         if (candidates[i].eliminated == false)
         {
-            if (candidates[i].votes < candidates[i-1].votes)
+            if (candidates[i].votes < min)
             {
                 min = i;
             }

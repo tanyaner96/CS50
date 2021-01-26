80ca7a148d13ffcedc2e0b46071926286dd30f05
tanyaner96 <tanyaner96@users.noreply.github.com>
2021-01-26 13:02:08 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -17,12 +17,22 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
             //float blue = image[i][j].rgbtBlue;
             //float green = image[i][j].rgbtGreen;
 
-            int average = round(image[i][j].rgbtRed + image[i][j].rgbtBlue + image[i][j].rgbtGreen)/3.0;
+            int average = round((image[i][j].rgbtRed + image[i][j].rgbtBlue + image[i][j].rgbtGreen)/3.0);
 
+
+
+
+            // if (i == 8 && j == 12) {
+            //     printf("red = %i\n", image[i][j].rgbtRed);
+            //     printf("blue = %i\n", image[i][j].rgbtBlue);
+            //     printf("green = %i\n", image[i][j].rgbtGreen);
+
+            //     printf("average = %i\n",average);
+            // }
             image[i][j].rgbtRed = average;
             image[i][j].rgbtBlue = average;
             image[i][j].rgbtGreen = average;
-            
+
         }
     }
     return;
@@ -157,7 +167,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
                 image[i][j].rgbtBlue = round(avg_blue);
                 image[i][j].rgbtGreen = round(avg_green);
             }
-            
+
             else if ((i+1) > height && (j+1) > width)
             {
                 avg_red = (copy[i-1][j-1].rgbtRed + copy[i-1][j].rgbtRed + copy[i][j-1].rgbtRed + copy[i][j].rgbtRed)/4;
@@ -168,7 +178,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
                 image[i][j].rgbtBlue = round(avg_blue);
                 image[i][j].rgbtGreen = round(avg_green);
             }
-            
+
             else if ((i-1) < 0) // sides?
             {
                 avg_red = (copy[i][j-1].rgbtRed + copy[i][j].rgbtRed + copy[i][j+1].rgbtRed + copy[i+1][j-1].rgbtRed + copy[i+1][j].rgbtRed + copy[i+1][j+1].rgbtRed)/6;
@@ -179,7 +189,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
                 image[i][j].rgbtBlue = round(avg_blue);
                 image[i][j].rgbtGreen = round(avg_green);
             }
-            
+
             else if ((j-1) < 0)
             {
                 avg_red = (copy[i-1][j].rgbtRed + copy[i-1][j+1].rgbtRed + copy[i][j].rgbtRed + copy[i][j+1].rgbtRed + copy[i+1][j].rgbtRed + copy[i+1][j+1].rgbtRed)/6;
@@ -190,7 +200,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
                 image[i][j].rgbtBlue = round(avg_blue);
                 image[i][j].rgbtGreen = round(avg_green);
             }
-            
+
             else if ((i+1) > height)
             {
                 avg_red = (copy[i-1][j-1].rgbtRed + copy[i-1][j].rgbtRed + copy[i-1][j+1].rgbtRed + copy[i][j-1].rgbtRed + copy[i][j].rgbtRed + copy[i][j+1].rgbtRed)/6;
@@ -201,7 +211,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
                 image[i][j].rgbtBlue = round(avg_blue);
                 image[i][j].rgbtGreen = round(avg_green);
             }
-            
+
             else if ((j+1) > width)
             {
                 avg_red = (copy[i-1][j-1].rgbtRed + copy[i-1][j].rgbtRed + copy[i][j-1].rgbtRed + copy[i][j].rgbtRed + copy[i][j+1].rgbtRed + copy[i+1][j-1].rgbtRed)/6;
@@ -212,8 +222,8 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
                 image[i][j].rgbtBlue = round(avg_blue);
                 image[i][j].rgbtGreen = round(avg_green);
             }
-            
-            else 
+
+            else
             {
                 avg_red = (copy[i-1][j-1].rgbtRed + copy[i-1][j].rgbtRed + copy[i-1][j+1].rgbtRed + copy[i][j-1].rgbtRed + copy[i][j].rgbtRed + copy[i][j+1].rgbtRed + copy[i+1][j-1].rgbtRed + copy[i+1][j].rgbtRed + copy[i+1][j+1].rgbtRed)/9;
                 avg_blue = (copy[i-1][j-1].rgbtBlue + copy[i-1][j].rgbtBlue + copy[i-1][j+1].rgbtBlue + copy[i][j-1].rgbtBlue + copy[i][j].rgbtBlue + copy[i][j+1].rgbtBlue + copy[i+1][j-1].rgbtBlue + copy[i+1][j].rgbtBlue + copy[i+1][j+1].rgbtBlue)/9;

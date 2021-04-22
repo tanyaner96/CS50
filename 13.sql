12b0ce23d206b3d6e8994447484d6fe06c213b9d
tanyaner96 <tanyaner96@users.noreply.github.com>
2021-04-22 09:22:46 +0000

    automated commit by check50 [check50=True]

--- 13.sql
+++ 13.sql
@@ -6,21 +6,14 @@
 SELECT name FROM people
 JOIN stars ON people.id = stars.person_id
 JOIN movies ON stars.movie_id = movies.id
-
 WHERE movies.id IN (SELECT movies.id FROM people
-
 JOIN stars ON people.id = stars.person_id
 JOIN movies ON stars.movie_id = movies.id
-
 WHERE name = 'Kevin Bacon' AND birth = '1958')
-
 AND name != 'Kevin Bacon';
 
 
 
 
-SELECT movies.id FROM movies
-JOIN stars ON stars.movie_id = movies.id
-JOIN people ON people.id = stars.person_id
-WHERE name = 'Kevin Bacon' AND birth = '1958';
+
 
